b48d40194f65ef2ab43875bfcdeb6888f7cad068
RedJyu <piotr.stezycki@protonmai.com>
2023-11-17 13:15:37 +0100

    hero

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,3 +1,21 @@
+import heroImg from './assets/hero.svg';
 export const Hero = () => {
-  return <section className='hero'>Hero</section>;
+  return (
+    <section className='hero'>
+      <div className='hero-center'>
+        <div className='hero-title'>
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+            alias et, tempore ea rerum quibusdam? Reiciendis nemo ex dolorum
+            modi, quidem unde dicta vitae quod, excepturi blanditiis non,
+            dolores explicabo.
+          </p>
+        </div>
+        <div className='img-container'>
+          <img src={heroImg} alt='hero image' className='img' />
+        </div>
+      </div>
+    </section>
+  );
 };
